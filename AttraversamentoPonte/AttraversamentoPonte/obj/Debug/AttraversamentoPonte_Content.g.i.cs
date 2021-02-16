35e03a7f9ca4d4b487939ab24974f4d6cccb6382
FrancescoTonelli <francesco.tonelli01@gmail.com>
2021-02-16 14:22:28 +0100

    Ricomincio: ricostruzione della grafica

--- AttraversamentoPonte/AttraversamentoPonte/obj/Debug/AttraversamentoPonte_Content.g.i.cs
+++ AttraversamentoPonte/AttraversamentoPonte/obj/Debug/AttraversamentoPonte_Content.g.i.cs
@@ -8,7 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("autodx.png")]
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("autosx.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("ponte.png")]
 
 
